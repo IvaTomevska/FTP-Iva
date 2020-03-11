7c041f58dde3a52e92cb121a8d159ae0f9350652
IvaTomevska <tomevskaiva@gmail.com>
2020-03-11 14:19:51 +0400

    Cleaned up the messy code, added a master set, only accepts multiple clients at this point

--- TCPClient.c
+++ TCPClient.c
@@ -25,7 +25,7 @@ int main()
     char *hello="Hello from client";
     char buff[1024]={0};
   
-    // socket create and varification 
+    //Socket create and varification 
     sockfd = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd == -1) { 
         printf("socket creation failed...\n"); 
@@ -35,12 +35,12 @@ int main()
         printf("Socket successfully created..\n"); 
     bzero(&servaddr, sizeof(servaddr)); 
   
-    // assign IP, PORT 
+    //Assign IP, PORT 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
     servaddr.sin_port = htons(PORT); 
   
-    // connect the client socket to server socket 
+    //Connect the client socket to server socket 
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
         printf("connection with the server failed...\n"); 
         exit(0); 
@@ -48,7 +48,7 @@ int main()
     else
         printf("connected to the server..\n");
 
-    while(1){
+    /*while(1){
     	printf("ftp> ");
 
     	//Read the input and store into str
@@ -85,7 +85,7 @@ int main()
 			/*printf("%s\n", tokens[0]);
 			printf("%s\n", tokens[1]);
 			strcat(tokens[0], tokens[1]);
-			printf("%s\n", tokens[0]);*/
+			printf("%s\n", tokens[0]);
 
 		}
 		else if(strcmp(tokens[0],"PASS")==0){
@@ -96,10 +96,10 @@ int main()
 				send(sockfd, tokens[1], strlen(tokens[1]), 0);
 				printf("SERVER SAID OK\n");
 			}
-		}
+		}*/
 
 
-    } 
+    //} 
 
     send(sockfd, hello, strlen(hello), 0);
     printf("Hello from client sent\n");
--- TCPServer.c
+++ TCPServer.c
@@ -13,42 +13,44 @@
 
 int main() 
 { 
-    int sockfd_master, connfd, len, valread, i, sockfd, max_sd;
-    int new_socket, client_socket[30], max_clients=30, activity; 
+    int sockfd_master, connfd, len, i, max_fd;
+    int new_socket, activity; 
     struct sockaddr_in servaddr, cliaddr; 
     char *hello = "Hello from server";
-    char buff[1024] = {0};//data buffer
+    char buff[1024];//data buffer
 
-    fd_set read_fd_set;
+    fd_set read_fd_set; //temp file desctiptor list for select()
+    fd_set sockfd_master_set; //master file descriptor list
 
-    //initialize client sockets to 0
-    for(i=0; i<max_clients; i++) 
-    	client_socket[i]=0;
+    //Slear the sets
+    FD_ZERO(&read_fd_set);
+    FD_ZERO(&sockfd_master_set);
   
-    // socket create and verification 
+    //Socket create and verification 
     sockfd_master = socket(AF_INET, SOCK_STREAM, 0); 
     if (sockfd_master == -1) { 
-        printf("socket creation failed...\n"); 
+        printf("Socket creation failed...\n"); 
         exit(0); 
     } 
     else
         printf("Socket successfully created..\n"); 
+    
     bzero(&servaddr, sizeof(servaddr)); 
   
-    // assign IP, PORT 
+    //Assign IP, PORT 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
     servaddr.sin_port = htons(PORT); 
   
-    // Binding newly created socket to given IP and verification 
+    //Binding newly created socket to given IP and verification 
     if ((bind(sockfd_master, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
+        printf("Socket bind failed...\n"); 
         exit(0); 
     } 
     else
         printf("Socket successfully binded..\n"); 
   
-    // Now server is ready to listen and verification 
+    //Now server is ready to listen and verification 
     if ((listen(sockfd_master, 5)) != 0) { 
         printf("Listen failed...\n"); 
         exit(0); 
@@ -58,94 +60,58 @@ int main()
     
     len = sizeof(cliaddr); 
   
-    // Accept the data packet from client and verification 
+    //Accept the data packet from client and verification 
     connfd = accept(sockfd_master, (SA*)&cliaddr, &len); 
     if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
+        printf("Server acccept failed...\n"); 
         exit(0); 
     } 
     else
-        printf("server acccept the client...\n"); 
+        printf("Server acccept the client...\n");
 
-    //valread = read (connfd, buff, 1024);
-    //printf("%s\n", buff);
+    //Add master socket to set
+    FD_SET(sockfd_master, &sockfd_master_set);
+    max_fd = sockfd_master; 
+
+
     send(connfd, hello, strlen(hello), 0);
     printf("Hello from server sent\n");
 
     while(1)
     {
-    	//clear the set
-    	FD_ZERO(&read_fd_set);
-
-    	//add master socket to set
-    	FD_SET(sockfd_master, &read_fd_set);
-    	max_sd = sockfd_master;
-
-    	//add child socket to set
-    	for(i=0; i<max_clients; i++) {
-    		sockfd = client_socket[i];
-
-    		//check validity
-    		if(sockfd > 0)
-    			FD_SET(sockfd, &read_fd_set);
-
-    		//highest fd number (for select ftn)
-    		if(sockfd > max_sd)
-    			max_sd = sockfd;
-    	}
-
-    	//wait for activity
-    	activity = select(max_sd + 1, &read_fd_set, NULL, NULL, NULL);
+        read_fd_set = sockfd_master_set;
+    	
+    	//Wait for activity
+    	activity = select(max_fd + 1, &read_fd_set, NULL, NULL, NULL);
 
     	if(activity < 0)
     		printf("Select function error\n");
 
-    	/*If something happened to mamaster socket
-    	then it is an incoming connection*/
-    	if(FD_ISSET(sockfd_master, &read_fd_set)){
-    		if ((new_socket = accept(sockfd_master, (SA*)&cliaddr, &len))<0) {
-    			perror("accept");
-    			exit(EXIT_FAILURE);
-    		}
+        //Run through the existing connections looking for data to read
+        for(i=0; i <= max_fd; i++) {
+            if(FD_ISSET(i, &read_fd_set)) {
+                if(i == sockfd_master) {
+                    //Handle new connections
+                    len = sizeof(cliaddr);
 
-    		//send new connection hello message
-    		if(send(new_socket, hello, strlen(hello), 0) != strlen(hello))
-    			perror("send");
+                    if((new_socket = accept(sockfd_master, (SA*)&cliaddr, &len))<0) {
+                        perror("Accept function error\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    else {
+                        FD_SET(new_socket, &sockfd_master_set);
 
-    		printf("Hello message sent succesfully\n");
+                        //Keep track of highest fd number
+                        if(new_socket > max_fd)
+                            max_fd = new_socket;
 
-    		//add new socket to array and listen to it's message
-    		for(i=0; i<max_clients; i++) {
-    			if(client_socket[i] == 0) {
-    				client_socket[i] = new_socket;
-    				printf("Adding to list of sockets as %d\n", i);
-    				valread = read (client_socket[i], buff, 1024);
-	    			//printf("%s\n", buff);
-	    			char *ok = "Command ok";
-	    			if(strcmp(buff, "USER")==0)
-	    			{
-	    				memset(buff, 0, sizeof(buff));
-	    				valread=0;
-	    				send(client_socket[i], ok, strlen(ok), 0);
-	    				//valread = read (client_socket[i], buff, 1024);
-	    				printf("Buffer is %s\n", buff);
-
-	    			}
-	    			else if(strcmp(buff, "PASS")==0)
-	    			{
-	    				memset(buff, 0, sizeof(buff));
-	    				valread=0;
-	    				send(client_socket[i], ok, strlen(ok), 0);
-	    				//valread = read (client_socket[i], buff, 1024);
-	    				printf("Buffer is %s\n", buff);
-
-	    			}
-    				break;
-    			}
-    		}
-    	}
+                        printf("New connection from %d on socket %d\n", inet_ntoa(cliaddr.sin_addr), new_socket);
+                    }
+                }
+            }
+        }	
     }
   
     //Close the socket 
-    close(sockfd); 
+    close(sockfd_master); 
 }
